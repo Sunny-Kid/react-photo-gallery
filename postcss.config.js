cc227587a9397bca0bfc5b067ef3e92e01513d97
hank.ren <987942918@qq.com>
2019-10-05 10:33:05 +0800

    feat: update postcsss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer')({
-      browsers: ['Chrome > 31', 'ff > 31', 'ie >= 10'],
+      overrideBrowserslist: ['Chrome > 31', 'ff > 31', 'ie >= 10'],
     }),
   ],
 };
